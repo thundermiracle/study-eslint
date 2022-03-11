c29211c2199493792286ea0e5516674005987244
ThunderMiracle <ho.shinjo@justincase-tech.com>
2022-03-11 18:09:18 +0900

    test: add verifyAndFix test

--- packages/eslint-plugin/src/test/ast-learning.spec.js
+++ packages/eslint-plugin/src/test/ast-learning.spec.js
@@ -1,4 +1,5 @@
-import { ESLintUtils } from '@typescript-eslint/utils';
+import { ESLintUtils, TSESLint } from '@typescript-eslint/utils';
+import * as parser from '@typescript-eslint/parser';
 import rule from '../rules/ast-learning';
 
 const ruleTester = new ESLintUtils.RuleTester({
@@ -28,4 +29,45 @@ ruleTester.run('my-rule', rule, {
       output: 'const foo = shallow<typeof div>(<div />);',
     },
   ],
+});
+
+describe('ast-learning', () => {
+  const linter = new TSESLint.Linter();
+  linter.defineRule('ast-learning', rule);
+  linter.defineParser('@typescript-eslint/parser', parser);
+
+  function testOutput(code, output) {
+    it(code, () => {
+      const result = linter.verifyAndFix(
+        code,
+        {
+          rules: {
+            'ast-learning': ['error'],
+          },
+          parser: '@typescript-eslint/parser',
+          parserOptions: {
+            ecmaFeatures: {
+              jsx: true,
+            },
+          },
+        },
+        {
+          fix: true,
+        },
+      );
+
+      expect(result.messages).toHaveLength(0);
+      expect(result.output).toBe(output);
+    });
+  }
+
+  testOutput(
+    'const foo = shallow<typeof div>(<div />);',
+    'const foo = shallow<typeof div>(<div />);',
+  );
+
+  testOutput(
+    'const foo = shallow(<div />);',
+    'const foo = shallow<typeof div>(<div />);',
+  );
 });
